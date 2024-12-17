e63962a6c02faeaa9e783ef7e858f0a022ffc4c8
Howard Henson <howard@henson.me.uk>
2024-12-17 21:55:31 +0400

    Cleanup of node

--- include/hgraph/types/node.h
+++ include/hgraph/types/node.h
@@ -2,85 +2,80 @@
 #ifndef NODE_H
 #define NODE_H
 
-#include <hgraph/python/pyb.h>
+#include <algorithm>
 #include <hgraph/hgraph_export.h>
-#include <vector>
+#include <hgraph/python/pyb.h>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <algorithm>
-#include <sstream>
+#include <vector>
 
+#include <hgraph/util/date_time.h>
 #include <hgraph/util/lifecycle.h>
 #include <nanobind/intrusive/ref.h>
-#include <hgraph/util/date_time.h>
 
-namespace hgraph {
+namespace hgraph
+{
     struct Graph;
+    using graph_ptr = nanobind::ref<Graph>;
 
-    template<typename Enum>
-    typename std::enable_if<std::is_enum<Enum>::value, Enum>::type
-    operator|(Enum lhs, Enum rhs) {
+    template <typename Enum> typename std::enable_if<std::is_enum<Enum>::value, Enum>::type operator|(Enum lhs, Enum rhs) {
         using underlying = typename std::underlying_type<Enum>::type;
-        return static_cast<Enum>(
-            static_cast<underlying>(lhs) | static_cast<underlying>(rhs)
-        );
+        return static_cast<Enum>(static_cast<underlying>(lhs) | static_cast<underlying>(rhs));
     }
 
-    template<typename Enum>
-    typename std::enable_if<std::is_enum<Enum>::value, Enum>::type
-    operator&(Enum lhs, Enum rhs) {
+    template <typename Enum> typename std::enable_if<std::is_enum<Enum>::value, Enum>::type operator&(Enum lhs, Enum rhs) {
         using underlying = typename std::underlying_type<Enum>::type;
-        return static_cast<Enum>(
-            static_cast<underlying>(lhs) & static_cast<underlying>(rhs)
-        );
+        return static_cast<Enum>(static_cast<underlying>(lhs) & static_cast<underlying>(rhs));
     }
 
     enum class NodeTypeEnum : char8_t {
-        NONE = 0,
-        SOURCE_NODE = 1,
+        NONE             = 0,
+        SOURCE_NODE      = 1,
         PUSH_SOURCE_NODE = SOURCE_NODE | (1 << 1),
         PULL_SOURCE_NODE = SOURCE_NODE | (1 << 2),
-        COMPUTE_NODE = 1 << 3,
-        SINK_NODE = 1 << 4
+        COMPUTE_NODE     = 1 << 3,
+        SINK_NODE        = 1 << 4
     };
 
     void node_type_enum_py_register(nb::module_ &m);
 
     enum class InjectableTypesEnum : char8_t {
-        NONE = 0,
-        STATE = 1,
-        SCHEDULER = 1 << 1,
-        OUTPUT = 1 << 2,
-        CLOCK = 1 << 3,
-        ENGINE_API = 1 << 4,
+        NONE         = 0,
+        STATE        = 1,
+        SCHEDULER    = 1 << 1,
+        OUTPUT       = 1 << 2,
+        CLOCK        = 1 << 3,
+        ENGINE_API   = 1 << 4,
         REPLAY_STATE = 1 << 5,
-        LOGGER = 1 << 6
+        LOGGER       = 1 << 6
     };
 
     void injectable_type_enum(nb::module_ &m);
 
-    struct HGRAPH_EXPORT NodeSignature {
+    struct HGRAPH_EXPORT NodeSignature
+    {
         using ptr = nanobind::ref<NodeSignature>;
 
-        std::string name{};
-        NodeTypeEnum node_type{NodeTypeEnum::NONE};
-        std::vector<std::string> args{};
-        std::optional<std::unordered_map<std::string, nb::object> > time_series_inputs{};
-        std::optional<nb::object> time_series_output{};
-        std::optional<std::unordered_map<std::string, nb::object> > scalars{};
-        nb::object src_location{nb::none()};
-        std::optional<std::unordered_set<std::string> > active_inputs{};
-        std::optional<std::unordered_set<std::string> > valid_inputs{};
-        std::optional<std::unordered_set<std::string> > all_valid_inputs{};
-        std::optional<std::unordered_set<std::string> > context_inputs{};
-        InjectableTypesEnum injectable_inputs{InjectableTypesEnum::NONE};
-        std::string wiring_path_name{};
-        std::optional<std::string> label{};
-        std::optional<std::string> record_replay_id{};
-        bool capture_values{false};
-        bool capture_exception{false};
-        char8_t trace_back_depth{1};
+        std::string                                                name{};
+        NodeTypeEnum                                               node_type{NodeTypeEnum::NONE};
+        std::vector<std::string>                                   args{};
+        std::optional<std::unordered_map<std::string, nb::object>> time_series_inputs{};
+        std::optional<nb::object>                                  time_series_output{};
+        std::optional<std::unordered_map<std::string, nb::object>> scalars{};
+        nb::object                                                 src_location{nb::none()};
+        std::optional<std::unordered_set<std::string>>             active_inputs{};
+        std::optional<std::unordered_set<std::string>>             valid_inputs{};
+        std::optional<std::unordered_set<std::string>>             all_valid_inputs{};
+        std::optional<std::unordered_set<std::string>>             context_inputs{};
+        InjectableTypesEnum                                        injectable_inputs{InjectableTypesEnum::NONE};
+        std::string                                                wiring_path_name{};
+        std::optional<std::string>                                 label{};
+        std::optional<std::string>                                 record_replay_id{};
+        bool                                                       capture_values{false};
+        bool                                                       capture_exception{false};
+        char8_t                                                    trace_back_depth{1};
 
         [[nodiscard]] nb::object get_arg_type(const std::string &arg) const;
 
@@ -118,34 +113,36 @@ namespace hgraph {
     struct TimeSeriesBundleOutput;
     struct TimeSeriesOutput;
 
-    struct NodeScheduler {
+    struct NodeScheduler
+    {
         [[nodiscard]] engine_time_t next_scheduled_time() const;
-        [[nodiscard]] bool is_scheduled() const;
-        [[nodiscard]] bool is_scheduled_node() const;
-        [[nodiscard]] bool has_tag(const std::string& tag) const;
-        engine_time_t pop_tag(const std::string& tag, std::optional<engine_time_t> default_time);
-        void schedule(engine_time_t when, std::optional<std::string> tag);
-        void schedule(engine_time_delta_t when, std::optional<std::string> tag);
-        void un_schedule(std::optional<std::string> tag);
-        void reset();
+        [[nodiscard]] bool          is_scheduled() const;
+        [[nodiscard]] bool          is_scheduled_node() const;
+        [[nodiscard]] bool          has_tag(const std::string &tag) const;
+        engine_time_t               pop_tag(const std::string &tag, std::optional<engine_time_t> default_time);
+        void                        schedule(engine_time_t when, std::optional<std::string> tag);
+        void                        schedule(engine_time_delta_t when, std::optional<std::string> tag);
+        void                        un_schedule(std::optional<std::string> tag);
+        void                        reset();
     };
 
-    struct HGRAPH_EXPORT Node : ComponentLifeCycle {
+    struct HGRAPH_EXPORT Node : ComponentLifeCycle
+    {
         using ptr = nanobind::ref<Node>;
 
         Node(int64_t node_ndx, std::vector<int64_t> owning_graph_id, NodeSignature signature, nb::dict scalars);
 
-        int64_t _node_ndx;
-        std::vector<int64_t> _owning_graph_id;
-        std::vector<int64_t> _node_id;
-        NodeSignature::ptr _signature;
-        nb::dict _scalars;
-        Graph::ptr _graph;
-        nb::ref<TimeSeriesBundleInput> _input;
-        nb::ref<TimeSeriesOutput> _output;
-        nb::ref<TimeSeriesOutput> _error_output;
+        int64_t                         _node_ndx;
+        std::vector<int64_t>            _owning_graph_id;
+        std::vector<int64_t>            _node_id;
+        NodeSignature::ptr              _signature;
+        nb::dict                        _scalars;
+        graph_ptr                       _graph;
+        nb::ref<TimeSeriesBundleInput>  _input;
+        nb::ref<TimeSeriesOutput>       _output;
+        nb::ref<TimeSeriesOutput>       _error_output;
         nb::ref<TimeSeriesBundleOutput> _recordable_state;
-        std::optional<NodeScheduler> _scheduler;
+        std::optional<NodeScheduler>    _scheduler;
 
         std::vector<nb::ref<TimeSeriesInput>> start_inputs() const;
 
@@ -153,37 +150,36 @@ namespace hgraph {
         virtual void notify(engine_time_t modified_time);
         virtual void notify_next_cycle();
 
-        int64_t node_ndx() const { return _node_ndx; }
+        int64_t node_ndx() const;
 
-        const std::vector<int64_t> &owning_graph_id() const { return _owning_graph_id; }
+        const std::vector<int64_t> &owning_graph_id() const;
 
-        const std::vector<int64_t> &node_id() const { return _node_id; }
+        const std::vector<int64_t> &node_id() const;
 
-        const NodeSignature::ptr &signature() const { return _signature; }
+        const NodeSignature::ptr &signature() const;
 
-        const nb::dict &scalars() const { return _scalars; }
+        const nb::dict &scalars() const;
 
-        Graph &graph() const { return *_graph; }
+        Graph &graph() const;
 
-        void set_graph(Graph::ptr value) { _graph = std::move(value); }
+        void set_graph(graph_ptr value);
 
-        TimeSeriesBundleInput &input() const { return *_input; }
+        TimeSeriesBundleInput &input() const;
 
-        void set_input(nb::ref<TimeSeriesBundleInput> value) { _input = value; }
+        void set_input(nb::ref<TimeSeriesBundleInput> value);
 
-        nb::ref<TimeSeriesOutput> output() const { return _output; }
+        nb::ref<TimeSeriesOutput> output() const;
 
-        void set_output(nb::ref<TimeSeriesOutput> value) { _output = value; }
+        void set_output(nb::ref<TimeSeriesOutput> value);
 
-        nb::ref<TimeSeriesBundleOutput> recordable_state() const { return _recordable_state; }
+        nb::ref<TimeSeriesBundleOutput> recordable_state() const;
 
-        void set_recordable_state(nb::ref<TimeSeriesBundleOutput> value) { _recordable_state = value; }
+        void set_recordable_state(nb::ref<TimeSeriesBundleOutput> value);
 
-        std::optional<NodeScheduler> get_scheduler() const { return _scheduler; }
+        std::optional<NodeScheduler> scheduler() const;
 
-        nb::ref<TimeSeriesOutput> get_error_output() const { return _error_output; }
-
+        nb::ref<TimeSeriesOutput> error_output() const;
     };
-}
+}  // namespace hgraph
 
-#endif //NODE_H
+#endif  // NODE_H
--- src/cpp/types/node.cpp
+++ src/cpp/types/node.cpp
@@ -1,54 +1,48 @@
 #include <hgraph/python/pyb_wiring.h>
+#include <hgraph/types/graph.h>
 #include <hgraph/types/node.h>
 
-namespace hgraph {
+namespace hgraph
+{
     void node_type_enum_py_register(nb::module_ &m) {
         nb::enum_<NodeTypeEnum>(m, "NodeTypeEnum")
-                .value("SOURCE_NODE", NodeTypeEnum::SOURCE_NODE)
-                .value("PUSH_SOURCE_NODE", NodeTypeEnum::PUSH_SOURCE_NODE)
-                .value("PULL_SOURCE_NODE", NodeTypeEnum::PULL_SOURCE_NODE)
-                .value("COMPUTE_NODE", NodeTypeEnum::COMPUTE_NODE)
-                .value("SINK_NODE", NodeTypeEnum::SINK_NODE)
-                .export_values();
+            .value("SOURCE_NODE", NodeTypeEnum::SOURCE_NODE)
+            .value("PUSH_SOURCE_NODE", NodeTypeEnum::PUSH_SOURCE_NODE)
+            .value("PULL_SOURCE_NODE", NodeTypeEnum::PULL_SOURCE_NODE)
+            .value("COMPUTE_NODE", NodeTypeEnum::COMPUTE_NODE)
+            .value("SINK_NODE", NodeTypeEnum::SINK_NODE)
+            .export_values();
     }
 
     void injectable_type_enum(nb::module_ &m) {
         nb::enum_<InjectableTypesEnum>(m, "InjectableTypes")
-                .value("STATE", InjectableTypesEnum::STATE)
-                .value("SCHEDULER", InjectableTypesEnum::SCHEDULER)
-                .value("OUTPUT", InjectableTypesEnum::OUTPUT)
-                .value("CLOCK", InjectableTypesEnum::CLOCK)
-                .value("ENGINE_API", InjectableTypesEnum::ENGINE_API)
-                .value("REPLAY_STATE", InjectableTypesEnum::REPLAY_STATE)
-                .value("LOGGER", InjectableTypesEnum::LOGGER)
-                .export_values();
+            .value("STATE", InjectableTypesEnum::STATE)
+            .value("SCHEDULER", InjectableTypesEnum::SCHEDULER)
+            .value("OUTPUT", InjectableTypesEnum::OUTPUT)
+            .value("CLOCK", InjectableTypesEnum::CLOCK)
+            .value("ENGINE_API", InjectableTypesEnum::ENGINE_API)
+            .value("REPLAY_STATE", InjectableTypesEnum::REPLAY_STATE)
+            .value("LOGGER", InjectableTypesEnum::LOGGER)
+            .export_values();
     }
 
     [[nodiscard]] nb::object NodeSignature::get_arg_type(const std::string &arg) const {
-        if (time_series_inputs && time_series_inputs->contains(arg)) {
-            return time_series_inputs->at(arg);
-        }
-        if (scalars && scalars->contains(arg)) {
-            return scalars->at(arg);
-        }
+        if (time_series_inputs && time_series_inputs->contains(arg)) { return time_series_inputs->at(arg); }
+        if (scalars && scalars->contains(arg)) { return scalars->at(arg); }
         return nb::none();
     }
 
     [[nodiscard]] std::string NodeSignature::signature() const {
         std::ostringstream oss;
-        bool first = true;
-        auto none_str{std::string("None")};
+        bool               first = true;
+        auto               none_str{std::string("None")};
 
         oss << name << "(";
 
-        auto obj_to_type = [&](const nb::object &obj) {
-            return obj.is_none() ? none_str : nb::cast<std::string>(obj);
-        };
+        auto obj_to_type = [&](const nb::object &obj) { return obj.is_none() ? none_str : nb::cast<std::string>(obj); };
 
-        for (const auto &arg: args) {
-            if (!first) {
-                oss << ", ";
-            }
+        for (const auto &arg : args) {
+            if (!first) { oss << ", "; }
             oss << arg << ": " << obj_to_type(get_arg_type(arg));
             first = false;
         }
@@ -107,60 +101,77 @@ namespace hgraph {
         return (node_type & NodeTypeEnum::SINK_NODE) == NodeTypeEnum::SINK_NODE;
     }
 
-    [[nodiscard]] bool NodeSignature::is_recordable() const {
-        return (bool) record_replay_id;
-    }
+    [[nodiscard]] bool NodeSignature::is_recordable() const { return (bool)record_replay_id; }
 
     void NodeSignature::py_register(nb::module_ &m) {
-        nb::class_<NodeSignature >(m, "NodeSignature")
-                .def(nb::init<std::string,
-                         NodeTypeEnum,
-                         std::vector<std::string>,
-                         std::optional<std::unordered_map<std::string, nb::object> >,
-                         std::optional<nb::object>,
-                         std::optional<std::unordered_map<std::string, nb::object> >,
-                         nb::object,
-                         std::optional<std::unordered_set<std::string> >,
-                         std::optional<std::unordered_set<std::string> >,
-                         std::optional<std::unordered_set<std::string> >,
-                         std::optional<std::unordered_set<std::string> >,
-                         InjectableTypesEnum,
-                         std::string,
-                         std::optional<std::string>,
-                         std::optional<std::string>,
-                         bool,
-                         bool,
-                         char8_t>(),
-                     "name"_a, "node_type"_a, "args"_a, "time_series_inputs"_a, "time_series_output"_a,
-                     "scalars"_a, "src_location"_a, "active_inputs"_a, "valid_inputs"_a,
-                     "all_valid_inputs"_a, "context_inputs"_a, "injectable_inputs"_a, "wiring_path_name"_a,
-                     "label"_a, "record_replay_id"_a, "capture_values"_a, "capture_exception"_a,
-                     "trace_back_depth"_a
-                )
-                .def_prop_ro("signature", &NodeSignature::signature)
-                .def_prop_ro("uses_scheduler", &NodeSignature::uses_scheduler)
-                .def_prop_ro("uses_clock", &NodeSignature::uses_clock)
-                .def_prop_ro("uses_engine", &NodeSignature::uses_engine)
-                .def_prop_ro("uses_state", &NodeSignature::uses_state)
-                .def_prop_ro("uses_output_feedback", &NodeSignature::uses_output_feedback)
-                .def_prop_ro("uses_replay_state", &NodeSignature::uses_replay_state)
-                .def_prop_ro("is_source_node", &NodeSignature::is_source_node)
-                .def_prop_ro("is_push_source_node", &NodeSignature::is_push_source_node)
-                .def_prop_ro("is_pull_source_node", &NodeSignature::is_pull_source_node)
-                .def_prop_ro("is_compute_node", &NodeSignature::is_compute_node)
-                .def_prop_ro("is_sink_node", &NodeSignature::is_sink_node)
-                .def_prop_ro("is_recordable", &NodeSignature::is_recordable)
-                .def("to_dict", [](const NodeSignature &self) {
-                    nb::dict d{};
-                    d["name"] = self.name;
-                    return d;
-                })
-                // .def("copy_with", [](const NodeSignature& self, py::kwargs kwargs) {
-                //     return std::shared_ptr<NodeSignature>(
-                //         kwargs.contains("name") ? py::cast<std::string>(kwargs["name"]) : self->name
-                //         ...
-                //     );
-                // })
-                ;
+        nb::class_<NodeSignature>(m, "NodeSignature")
+            .def(nb::init<std::string, NodeTypeEnum, std::vector<std::string>,
+                          std::optional<std::unordered_map<std::string, nb::object>>, std::optional<nb::object>,
+                          std::optional<std::unordered_map<std::string, nb::object>>, nb::object,
+                          std::optional<std::unordered_set<std::string>>, std::optional<std::unordered_set<std::string>>,
+                          std::optional<std::unordered_set<std::string>>, std::optional<std::unordered_set<std::string>>,
+                          InjectableTypesEnum, std::string, std::optional<std::string>, std::optional<std::string>, bool, bool,
+                          char8_t>(),
+                 "name"_a, "node_type"_a, "args"_a, "time_series_inputs"_a, "time_series_output"_a, "scalars"_a, "src_location"_a,
+                 "active_inputs"_a, "valid_inputs"_a, "all_valid_inputs"_a, "context_inputs"_a, "injectable_inputs"_a,
+                 "wiring_path_name"_a, "label"_a, "record_replay_id"_a, "capture_values"_a, "capture_exception"_a,
+                 "trace_back_depth"_a)
+            .def_prop_ro("signature", &NodeSignature::signature)
+            .def_prop_ro("uses_scheduler", &NodeSignature::uses_scheduler)
+            .def_prop_ro("uses_clock", &NodeSignature::uses_clock)
+            .def_prop_ro("uses_engine", &NodeSignature::uses_engine)
+            .def_prop_ro("uses_state", &NodeSignature::uses_state)
+            .def_prop_ro("uses_output_feedback", &NodeSignature::uses_output_feedback)
+            .def_prop_ro("uses_replay_state", &NodeSignature::uses_replay_state)
+            .def_prop_ro("is_source_node", &NodeSignature::is_source_node)
+            .def_prop_ro("is_push_source_node", &NodeSignature::is_push_source_node)
+            .def_prop_ro("is_pull_source_node", &NodeSignature::is_pull_source_node)
+            .def_prop_ro("is_compute_node", &NodeSignature::is_compute_node)
+            .def_prop_ro("is_sink_node", &NodeSignature::is_sink_node)
+            .def_prop_ro("is_recordable", &NodeSignature::is_recordable)
+            .def("to_dict",
+                 [](const NodeSignature &self) {
+                     nb::dict d{};
+                     d["name"] = self.name;
+                     return d;
+                 })
+            // .def("copy_with", [](const NodeSignature& self, py::kwargs kwargs) {
+            //     return std::shared_ptr<NodeSignature>(
+            //         kwargs.contains("name") ? py::cast<std::string>(kwargs["name"]) : self->name
+            //         ...
+            //     );
+            // })
+            ;
     }
-}
+
+    int64_t Node::node_ndx() const { return _node_ndx; }
+
+    const std::vector<int64_t> &Node::owning_graph_id() const { return _owning_graph_id; }
+
+    const std::vector<int64_t> &Node::node_id() const { return _node_id; }
+
+    const NodeSignature::ptr &Node::signature() const { return _signature; }
+
+    const nb::dict &Node::scalars() const { return _scalars; }
+
+    Graph &Node::graph() const { return *_graph; }
+
+    void Node::set_graph(graph_ptr value) { _graph = value; }
+
+    TimeSeriesBundleInput &Node::input() const { return *_input; }
+
+    void Node::set_input(nb::ref<TimeSeriesBundleInput> value) { _input = value; }
+
+    nb::ref<TimeSeriesOutput> Node::output() const { return _output; }
+
+    void Node::set_output(nb::ref<TimeSeriesOutput> value) { _output = value; }
+
+    nb::ref<TimeSeriesBundleOutput> Node::recordable_state() const { return _recordable_state; }
+
+    void Node::set_recordable_state(nb::ref<TimeSeriesBundleOutput> value) { _recordable_state = value; }
+
+    std::optional<NodeScheduler> Node::scheduler() const { return _scheduler; }
+
+    nb::ref<TimeSeriesOutput> Node::error_output() const { return _error_output; }
+
+}  // namespace hgraph
