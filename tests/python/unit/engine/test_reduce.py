5799890881415bf8667a219b725d297bed4566f6
Howard Henson <howard@henson.me.uk>
2025-10-05 15:29:07 +0400

    Add simple unit test for reduce

---
+++ tests/python/unit/engine/test_reduce.py
@@ -0,0 +1,11 @@
+from hgraph import graph, TS, TSD, reduce
+from hgraph.test import eval_node
+
+def test_reduce_simple():
+
+    @graph
+    def g(tsd: TSD[int, TS[int]]) -> TS[int]:
+        return reduce(lambda a, b: a+b, tsd, 0.0)
+    result = eval_node(g, [{1:1}, {2:2}, None, {1:3}])
+    print(f"result: {result}")
+    assert result == [1, 3, None, 5]
