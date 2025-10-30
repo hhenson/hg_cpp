2797120c4d4cb952a2d9480312e1ff9af63cb2cf
Howard Henson <howard@henson.me.uk>
2025-10-30 08:57:01 +0400

    fix duplicate registration of TSW builders

--- src/cpp/python/_hgraph_builder.cpp
+++ src/cpp/python/_hgraph_builder.cpp
@@ -4,8 +4,6 @@
 #include <hgraph/builders/output_builder.h>
 #include <hgraph/builders/node_builder.h>
 #include <hgraph/builders/graph_builder.h>
-#include <hgraph/builders/time_series_types/time_series_window_input_builder.h>
-#include <hgraph/builders/time_series_types/time_series_window_output_builder.h>
 
 void export_builders(nb::module_ &m) {
     using namespace hgraph;
@@ -14,6 +12,4 @@ void export_builders(nb::module_ &m) {
     InputBuilder::register_with_nanobind(m);
     NodeBuilder::register_with_nanobind(m);
     GraphBuilder::register_with_nanobind(m);
-    time_series_window_input_builder_register_with_nanobind(m);
-    time_series_window_output_builder_register_with_nanobind(m);
 }
