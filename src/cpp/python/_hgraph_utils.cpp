f36c92a0923a77769ad809421fea3177ea3391f0
Howard Henson <howard@henson.me.uk>
2025-10-30 11:50:58 +0400

    Expose MIN/MAX DT/ET/TD

--- src/cpp/python/_hgraph_utils.cpp
+++ src/cpp/python/_hgraph_utils.cpp
@@ -1,5 +1,6 @@
 
 #include <hgraph/util/lifecycle.h>
+#include <hgraph/util/date_time.h>
 #include <hgraph/python/global_keys.h>
 
 void export_utils(nb::module_ &m) {
@@ -7,4 +8,11 @@ void export_utils(nb::module_ &m) {
 
     ComponentLifeCycle::register_with_nanobind(m);
     OutputKeyBuilder::register_with_nanobind(m);
+
+    // Expose date/time constants
+    m.attr("MIN_DT") = MIN_DT;
+    m.attr("MAX_DT") = MAX_DT;
+    m.attr("MIN_ST") = MIN_ST;
+    m.attr("MAX_ET") = MAX_ET;
+    m.attr("MIN_TD") = MIN_TD;
 }
