0827f0fffedcc0002deec76ca0510e8c82c6cd6c
Howard Henson <howard@henson.me.uk>
2026-07-21 22:38:21 +0400

    Keep bridge records DSO-local

--- include/hgraph/python/bridge_state.h
+++ include/hgraph/python/bridge_state.h
@@ -55,7 +55,7 @@ using PyInferValueFn = void *; // set as Value (*)(nb::handle) by the module
  */
 [[nodiscard]] HGRAPH_EXPORT nb::dict &bundle_class_registry();
 
-struct NB_EXPORT_SHARED PyBundleClassInfo {
+struct PyBundleClassInfo {
   using Allocator = PyObject *(*)(PyTypeObject *, Py_ssize_t);
 
   nb::object type{};
